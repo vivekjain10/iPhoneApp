9ab55819f96d7f3f06afa41356909b5cb46b07fa
Vivek Jain <vivekjain10@gmail.com>
2012-04-07 16:13:30 +0530

    Initial Commit

---
+++ iPhoneApp/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  iPhoneApp
+//
+//  Created by Vivek Jain on 07/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iPhoneApp/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  iPhoneApp
+//
+//  Created by Vivek Jain on 07/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
